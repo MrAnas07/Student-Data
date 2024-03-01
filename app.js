067d94c840a0775361ca5eb86a76491a96cdef17
Mr Anas <154543858+MrAnas07@users.noreply.github.com>
2024-03-01 19:54:08 +0500

    Student Data Collection

---
+++ app.js
@@ -0,0 +1,83 @@
+// var arr = [1, "Anas", "Amir", "0021235", "SMIT",]
+// arr.push("Osaf", "Hurais",)
+
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+
+// document.write(arr)
+
+// document.write(`First Name Is ${arr[1]} <br> Last Name Is ${arr[2]}`)
+
+// var arr = [1, 2, 3, 4, 5, 6, "false", true, 18.2, "Anas"]
+
+// document.write(arr[7])
+
+// var arr = [1, "Anas", "Amir", "0021235", "SMIT"]
+// arr.push("Osaf", "Hurais")
+// arr.push("Amir", "Anas")
+
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+
+
+// document.write(`First Name Is ${arr[1]} <br> Father Name Is ${arr[2]}`)
+
+
+
+
+
+
+
+var arr = [1044, "Anas", "Amir", "021235", "SMIT"]
+arr.push("Osaf", "Hurais")
+arr.pop()
+arr.pop()
+
+
+document.write(`Student ID ${arr[0]} <br> Student Name ${arr[1]} <br> Father Name ${arr[2]} 
+<br> Student Roll Number ${arr[3]} <br> Institute ${arr[4]} `)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
